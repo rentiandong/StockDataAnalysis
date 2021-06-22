4a7f4031abff872f9776fe6420b345ab77872534
Rentian <rentiandong@outlook.com>
2021-06-22 18:52:42 -0500

    Update python format script

---
+++ format_all_python_files.sh
@@ -0,0 +1,1 @@
+yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' src
--- format_files.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-list_files = subprocess.run(['yapf', '-i', '--style={based_on_style: pep8, column_limit: 120}', 'main.py'], shell=True)
