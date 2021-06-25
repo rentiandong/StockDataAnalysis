2f07d2c2962db36a7a069ed21979c7b9128cd7de
Rentian <rentiandong@outlook.com>
2021-06-25 01:05:47 -0500

    Update yapf format script to include all files

--- format_all_python_files.sh
+++ format_all_python_files.sh
@@ -1,1 +1,2 @@
-yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' src
+yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' stock_data_analysis
+yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' stock_data_analysis_test
