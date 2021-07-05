565c24d65819a9e4f0b059196c84df545891131d
Rentian <rentiandong@outlook.com>
2021-07-04 21:31:16 -0500

    Include "main.py" in formatter script

--- format_all_python_files.sh
+++ format_all_python_files.sh
@@ -1,2 +1,3 @@
 yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' stock_data_analysis
 yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' stock_data_analysis_test
+yapf -i -r --style='{based_on_style: pep8, column_limit: 120}' main.py
